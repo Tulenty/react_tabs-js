a428e2b8cdd3c8d8c6444af8de1935408473b20d
missk <rostock.vasiljev@gmail.com>
2024-11-02 16:13:57 +0200

    fix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,14 +1,32 @@
-export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const currentTabId = tabs.some(tab => tab.id === activeTabId)
+    ? activeTabId
+    : tabs[0]?.id;
+return(
   <div className="tabs is-boxed">
     <ul>
       {tabs.map(tab => (
         <li
           key={tab.id}
-          className={tab.id === activeTabId ? 'is-active' : ''}
+          className={tab.id === currentTabId ? 'is-active' : ''}
           data-cy="Tab"
-          onClick={() => tab.id !== activeTabId && onTabSelected(tab.id)}
         >
-          <a href={`#${tab.id}`} data-cy="TabLink">
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={e => {
+              e.preventDefault();
+              if (tab.id !== currentTabId) onTabSelected(tab.id);
+            }}
+            onKeyDown={e => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                e.preventDefault();
+                if (tab.id !== currentTabId) onTabSelected(tab.id);
+              }
+            }}
+            role="button"
+            tabIndex={0}
+          >
             {tab.title}
           </a>
         </li>
@@ -18,4 +36,4 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
       {tabs.find(tab => tab.id === activeTabId)?.content}
     </div>
   </div>
-);
+)};
